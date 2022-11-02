bb9250a6d6b988cdce8e21c86632b06081ad0809
Selim Can Şimşek <selimcansimsek0@gmail.com>
2022-11-03 00:54:29 +0300

    create api models and endpoints

---
+++ MovieDB.Business/Providers/MovieDBApi/Models/Request/MovieDetailRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieDB.Business.Providers.MovieDBApi.Models.Request
+{
+    public class MovieDetailRequest 
+    {
+        public string MovieId { get; set; }
+        public string Language { get; set; }    
+    }
+}
---
+++ MovieDB.Business/Providers/MovieDBApi/Models/Response/MovieUpComingResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MovieDB.Business.Providers.MovieDBApi.Models.Response
+{
+    public class MovieUpComingResponse
+    {
+        public Dates dates { get; set; }
+        public int page { get; set; }
+        public List<MovieHeader> results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+}
